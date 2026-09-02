b98d98795d8a1898eff911b1cfe22b9d65c42cd3
anuragk285 <anuragkanukuntla009@gmail.com>
2026-09-02 07:33:02 +0530

    add pool in database.py

--- database.py
+++ database.py
@@ -7,7 +7,10 @@ class Base(DeclarativeBase):
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///smart-question-bank-database.db")
 
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, 
+                            pool_pre_ping=True,      
+                            pool_recycle=1800,
+                            pool_timeout=30)
 SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_db(): 
